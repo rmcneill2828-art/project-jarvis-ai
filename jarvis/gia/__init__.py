58286f0ffc358fc76c996eafcb0eef90b7f47d2e
rmcneill2828-art <rmcneill2828@gmail.com>
2026-07-06 10:45:53 +0100

    feat(gia): add bootstrap readiness capability

---
+++ jarvis/gia/__init__.py
@@ -0,0 +1,17 @@
+"""Guardian Instrumentation Agent engineering capability primitives."""
+
+from jarvis.gia.bootstrap import (
+    ArtefactReview,
+    EngineeringReadinessContext,
+    EngineeringRequest,
+    GiaBootstrap,
+    ReadinessState,
+)
+
+__all__ = [
+    "ArtefactReview",
+    "EngineeringReadinessContext",
+    "EngineeringRequest",
+    "GiaBootstrap",
+    "ReadinessState",
+]
