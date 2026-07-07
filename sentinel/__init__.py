b0d19a0767084f01ba5b278f0e17eb13a31ffcff
rmcneill2828-art <rmcneill2828@gmail.com>
2026-07-07 17:41:35 +0100

    Add Sentinel package exports

---
+++ sentinel/__init__.py
@@ -0,0 +1,17 @@
+"""Sentinel package exports."""
+
+from sentinel.core import (
+    SentinelDecision,
+    SentinelDecisionOutcome,
+    SentinelRequest,
+    SentinelResponse,
+    SentinelTrustGateway,
+)
+
+__all__ = [
+    "SentinelDecision",
+    "SentinelDecisionOutcome",
+    "SentinelRequest",
+    "SentinelResponse",
+    "SentinelTrustGateway",
+]
