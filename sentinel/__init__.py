41ae06ca51ddaefa325abb5329036a7ac3f47f53
rmcneill2828-art <rmcneill2828@gmail.com>
2026-07-07 21:51:09 +0100

    Export Sentinel provider abstraction API

--- sentinel/__init__.py
+++ sentinel/__init__.py
@@ -7,11 +7,23 @@ from sentinel.core import (
     SentinelResponse,
     SentinelTrustGateway,
 )
+from sentinel.providers import (
+    ExecutionProvider,
+    ProviderRegistry,
+    ProviderRequest,
+    ProviderResponse,
+    execute_with_sentinel_decision,
+)
 
 __all__ = [
+    "ExecutionProvider",
+    "ProviderRegistry",
+    "ProviderRequest",
+    "ProviderResponse",
     "SentinelDecision",
     "SentinelDecisionOutcome",
     "SentinelRequest",
     "SentinelResponse",
     "SentinelTrustGateway",
+    "execute_with_sentinel_decision",
 ]
